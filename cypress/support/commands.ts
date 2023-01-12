6b1a991c41e6387ae41c01e2185e55711a6291f4
user <user@users-mbp.home>
2023-01-12 01:44:12 +0100

    reformated code by prettier

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,78 +1,67 @@
 /// <reference types="cypress" />
 
 const cartIcon = () => {
-  return cy.get('#shopping_cart_container .shopping_cart_link');
+  return cy.get("#shopping_cart_container .shopping_cart_link");
 };
 
-Cypress.Commands.overwrite('visit', (originalFn, path, options) => {
+Cypress.Commands.overwrite("visit", (originalFn, path, options) => {
   // workaround for Cypress defect, more info // https://github.com/cypress-io/cypress/issues/16192
   originalFn(path, {
     onBeforeLoad(win) {
       // @ts-expect-error
       delete win.navigator.__proto__.serviceWorker;
     },
-    ...options
-  })
+    ...options,
+  });
 });
 
 Cypress.Commands.add("login", (userName, password) => {
-  cy.session([userName, password], () => {
-    cy.visit('/');
-    cy.get('[data-test="username"]').type(userName);
-    cy.get('[data-test="password"]').type(password);
-    cy.get('[data-test="login-button"]').click();
-    cy.url().should('include', '/inventory.html')
-  },
+  cy.session(
+    [userName, password],
+    () => {
+      cy.visit("/");
+      cy.get('[data-test="username"]').type(userName);
+      cy.get('[data-test="password"]').type(password);
+      cy.get('[data-test="login-button"]').click();
+      cy.url().should("include", "/inventory.html");
+    },
     {
       validate() {
-        cy.visit('/?/cart.html')
+        cy.visit("/?/cart.html");
       },
     }
-  )
+  );
 });
 
 Cypress.Commands.add("addProductToCart", (product) => {
-  cy.get(`#add-to-cart-${product}`)
-    .contains("Add to cart")
-    .click();
+  cy.get(`#add-to-cart-${product}`).contains("Add to cart").click();
 });
 
 Cypress.Commands.add("checkIfCartIconShowsNumberOfItems", (number) => {
-  cartIcon()
-  .contains(number);
+  cartIcon().contains(number);
 });
 
 Cypress.Commands.add("navigateToCart", () => {
-  cartIcon()
-  .click();
+  cartIcon().click();
 });
 
 Cypress.Commands.add("checkIfItemNameIsVisibleInTheCart", (productName) => {
-  cy.contains(productName)
-  .should('be.visible');
+  cy.contains(productName).should("be.visible");
 });
 
 Cypress.Commands.add("navigateToCheckout", () => {
-  cy.get('#checkout')
-  .click();
+  cy.get("#checkout").click();
 });
 
 Cypress.Commands.add("insertUsersData", (firstName, lastName, postalCode) => {
-   cy.get('#first-name')
-   .type(firstName);
-   cy.get('#last-name')
-   .type(lastName);
-   cy.get('#postal-code')
-   .type(postalCode);
-   cy.get('#continue')
-   .click();
+  cy.get("#first-name").type(firstName);
+  cy.get("#last-name").type(lastName);
+  cy.get("#postal-code").type(postalCode);
+  cy.get("#continue").click();
 });
 
 Cypress.Commands.add("checkProductInOwerview", (product) => {
-  cy.get('.cart_item')
-  .contains(product)
-  .scrollIntoView()
-  .should('be.visible');
+  cy.get(".cart_item").contains(product).scrollIntoView().should("be.visible");
 });
 
 Cypress.Commands.add("finishCheckout", () => {
@@ -80,7 +69,5 @@ Cypress.Commands.add("finishCheckout", () => {
 });
 
 Cypress.Commands.add("checkIfItemPurchasedSuccessfully", () => {
-  cy.contains("THANK YOU FOR YOUR ORDER")
-  .scrollIntoView()
-  .should('be.visible');
+  cy.contains("THANK YOU FOR YOUR ORDER").scrollIntoView().should("be.visible");
 });
